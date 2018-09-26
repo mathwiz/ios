c2d411578182686a40905b5f3fb25501634a2a14
mathwiz <mathwiz@gmail.com>
2018-09-26 12:44:36 -0400

    decorator

--- scala/src/pattern/structural/Asset.scala
+++ scala/src/pattern/structural/Asset.scala
@@ -3,19 +3,19 @@ package pattern.structural
 import pattern.SubclassResponsibility
 
 class Asset(val name: String, val id: String) {
-  var container: Asset = null
+  var container: Asset = _
 
   def value() : Double = {
-    throw new SubclassResponsibility()
+    throw SubclassResponsibility()
   }
 
   def containsSecurity(aSecurity: Asset) : Boolean = {
-    throw new SubclassResponsibility()
+    throw SubclassResponsibility()
   }
 
   def description() : String = s"$name ($id)"
 
-  def draw() = println(description())
+  def draw() : Unit = println(description())
 
   def depth() : Int = if (container == null) 0 else 1 + container.depth()
 

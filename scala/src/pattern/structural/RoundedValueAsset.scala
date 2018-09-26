20f5eeb233a2377f8a7496af8042be92567103f9
mathwiz <mathwiz@gmail.com>
2018-09-26 12:38:53 -0400

    composite

--- scala/src/pattern/structural/RoundedValueAsset.scala
+++ scala/src/pattern/structural/RoundedValueAsset.scala
@@ -1,7 +1,12 @@
 package pattern.structural
 
 class RoundedValueAsset(asset: Asset) extends AssetDecorator(asset) {
-  override def value()= {
+  override def value(): Double = {
     Math.round(asset .value())
+  }
+
+  override def toString(): String = {
+    println("toString in RoundedValueAsset " + asset .getClass)
+    asset .toString()
   }
 }
